847877433130d585f5e0491134fb3aecd05853e2
neera <kumarmanish150799@gmail.com>
2023-02-16 19:04:25 +0530

    routes and stripe done

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const PORT=process.env.port || 8000;
 const db = require('./config/mongoose');
 const userRoute=require("./routes/user");
 const authRoute=require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 dotenv.config();
 
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(PORT, function(err){
     if (err) {
